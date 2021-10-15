df5f4a0fb1b6d3c8c5fa0ece811e3fcab90cd77a
tnngoan <ngoan.n.tr@gmail.com>
2021-10-16 04:41:21 +0700

    add test

--- nearestPalette.js
+++ nearestPalette.js
@@ -1,12 +1,49 @@
-function nearestPalette(hex, colors) {
-  const distances = closestColor(toRGB(hex), colors);
-  const tops = distances.sort(function (a, b) {
-    return a.minD - b.minD;
+// top k sorted array of palettes
+module.exports = function nearestPalette(target, palettes, k) {
+  let nearestPalettes = [];
+  palettes.forEach((palette) => {
+    let minDistance = Number.MAX_VALUE;
+    palette.forEach((color) => {
+      let targetDistance = distanceToColor(target, color);
+      minDistance = Math.min(minDistance, targetDistance);
+    });
+    nearestPalettes.push({ distance: minDistance, palette: palette });
   });
-  return tops.slice(0, 10);
-}
+  sort(nearestPalettes);
+  return nearestPalettes.slice(0, k);
+};
 
-//hex to rgb converter
+//top k sorted array of every color in every palette
+module.exports = function nearestColor(target, palettes, k) {
+  let nearestColors = [];
+  palettes.forEach((palette) => {
+    let sortedColors = [];
+    let minDistance = Number.MAX_VALUE;
+    palette.forEach((color) => {
+      let targetDistance = distanceToColor(target, color);
+      minDistance = Math.min(minDistance, targetDistance);
+      sortedColors.push({ color: color, distance: targetDistance });
+    });
+    sort(sortedColors);
+    nearestColors.push({ distance: minDistance, colors: sortedColors });
+  });
+  return nearestColors.slice(0, k);
+};
+
+// calculate the distance between 2 colors
+module.exports = function distanceToColor(color1, color2) {
+  console.log("before rgb",color2)
+  color1 = toRGB(color1);
+  color2 = toRGB(color2);
+  console.log("see color2",color2);
+  const [r1, g1, b1] = color1;
+  const [r2, g2, b2] = color2;
+  const drp2 = Math.pow(r1 - r2, 2);
+  const dgp2 = Math.pow(g1 - g2, 2);
+  const dbp2 = Math.pow(b1 - b2, 2);
+  return Math.sqrt(drp2 + dgp2 + dbp2);
+};
+
 function toRGB(hex) {
   const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
   return result
@@ -18,32 +55,8 @@ function toRGB(hex) {
     : null;
 }
 
-//distance between 2 colors
-function colorDiff(color1, color2) {
-  const [r1, g1, b1] = color1;
-  const [r2, g2, b2] = color2;
-  const drp2 = Math.pow(r1 - r2, 2);
-  const dgp2 = Math.pow(g1 - g2, 2);
-  const dbp2 = Math.pow(b1 - b2, 2);
-  return Math.sqrt(drp2 + dgp2 + dbp2);
-}
-
-function closestColor(target, colors) {
-  let res = [];
-  colors.forEach((palette) => {
-    let minDistance = Number.MAX_VALUE;
-    let tD = [];
-    palette.forEach((color) => {
-      let targetDistance = colorDiff(target, toRGB(color));
-      minDistance = Math.min(minDistance, targetDistance);
-      tD.push({ targetDistance, color });
-    });
-    tD.sort(function (a, b) {
-      return b.targetDistance - a.targetDistance;
-    });
-    res.push({ minD: minDistance, pal: tD });
+function sort(array) {
+  array.sort(function (a, b) {
+    return a.distance - b.distance;
   });
-  return res;
 }
-
-export default nearestPalette;
--- nearestPalette.test.js
+++ nearestPalette.test.js
@@ -1,19 +1,61 @@
-import nearestPalette from "./nearestPalette";
+const { expect } = require("@jest/globals");
 
-let colors1 = [target, "#1F0001"];
-let colors2 = ["#2FFFFF", "#2FFFF0"];
-let palettes = [colors1, colors2];
-let expectedResult = [
-  {
-    distance: 0,
-    color: colors1,
-  },
-  {
-    distance: 350.54386316123123,
-    color: colors2,
-  },
+const nearestPalette = require("./nearestPalette");
+const nearestColor = require("./nearestPalette");
+const distanceToColor = require("./nearestPalette");
+
+const target = "#FFFFFF";
+const k = 2;
+const palettes = [
+  ["#69d2e7", "#a7dbd8", "#e0e4cc", "#f38630", "#fa6900"],
+  [target, "#cbe86b", "#f2e9e1", "#1c140d", "#cbe86b"],
+  ["#fe4365", "#fc9d9a", "#f9cdad", "#c8c8a9", "#83af9b"],
 ];
 
-test("PASS", () => {
-  expect(nearestPalette(target, palettes).toBe(expectedResult));
+test("TEST Distance", () => {
+  const res = distanceToColor(target, "#f2e9e1");
+  const er = 39.408120990476064;
+  expect(res).toBe(er);
+});
+
+test("TEST nearest Palettes", () => {
+  const res = nearestPalette(target, palettes, k);
+  let er = [
+    {
+      distance: 0,
+      palette: ["#FFFFFF", "#cbe86b", "#f2e9e1", "#1c140d", "#cbe86b"],
+    },
+    {
+      distance: 65.50572494064927,
+      palette: ["#69d2e7", "#a7dbd8", "#e0e4cc", "#f38630", "#fa6900"],
+    },
+  ];
+  expect(res).toBe(er);
+});
+
+test("TEST Nearest Colors", () => {
+  let res = nearestColor(target, palettes, k);
+  let er = [
+    {
+      distance: 65.50572494064927,
+      colors: [
+        { color: "#FFFFFF", distance: 0 },
+        { color: "#f2e9e1", distance: 39.408120990476064 },
+        { color: "#cbe86b", distance: 158.54652314068574 },
+        { color: "#cbe86b", distance: 158.54652314068574 },
+        { color: "#1c140d", distance: 406.59316275608967 },
+      ],
+    },
+    {
+      distance: 0,
+      colors: [
+        { color: "#e0e4cc", distance: 65.50572494064927 },
+        { color: "#a7dbd8", distance: 102.76672613253767 },
+        { color: "#69d2e7", distance: 158.43295111813072 },
+        { color: "#f38630", distance: 240.0708228835816 },
+        { color: "#fa6900", distance: 295.8884925102698 },
+      ],
+    },
+  ];
+  expect(res).toBe(er);
 });
